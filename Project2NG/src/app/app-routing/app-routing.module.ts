07fd04bb01bdc711a431716c14cfcfd300fddf61
unknown <jrufox@gmail.com>
2018-03-20 23:10:42 -0400

    first

---
+++ Project2NG/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from '../app.component';
+import { HomeComponent } from '../home/home.component'
+import { AboutComponent } from '../about/about.component'
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about/:id',
+    component: AboutComponent,
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ], exports: [
+      RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
