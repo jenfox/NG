701bf7acd084f1adf9371af709f7dcc45511ef0e
JDRyder <36388124+JDRyder@users.noreply.github.com>
2018-03-22 16:10:34 -0400

    Ryder j (#4)
    * filled out profile page and added dummy data, added form to profile to edit profile
    
    * edit profile functionality and fixed pathing of a tags in navbar to use ./

--- Project2NG/src/app/profile/profile.component.ts
+++ Project2NG/src/app/profile/profile.component.ts
@@ -10,18 +10,33 @@ export class ProfileComponent implements OnInit {
 
   uProfilePic:string = 'http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg';
   uEmail: string = '';
-  uFirstName:string='';
-  uLastName:string='';
-  uGender:string='';
-  uOrientation:string='';
+  uFirstName:string='Hermione';
+  uLastName:string='Granger';
+  uGender:string='Female';
+  uOrientation:string='Straight';
   uDOB: Date = null;
-  uPhoneNumber:string = ''
-  
+  uPhoneNumber:string = '(123) 456-7890';
+  editMode: boolean = false;
   constructor(public navbarService:NavbarService) { }
 
   ngOnInit() {
   
     this.navbarService.show();
   }
+
+editModeToggle(){
+  if (this.editMode)
+  {
+    this.editMode=false;
+  }else{
+    this.editMode=true;
+  }
+}
+
+  //on update of form, need to send a post to db of new info.
+  
+  //need live update of fields from db.
+
+  //onclick for reset password link needs to send password reset email. 
 
 }
