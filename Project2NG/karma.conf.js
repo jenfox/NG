bfdf3ae743af9415867e040e99b4aeb2ad1f3a4a
Matthew Butler <mattbutler737@gmail.com>
2018-03-24 20:14:47 -0400

    Update karma.conf.js

--- Project2NG/karma.conf.js
+++ Project2NG/karma.conf.js
@@ -10,6 +10,9 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      'karma-coverage',
+      'karma-phantomjs-launcher',
+      'karma-junit-reporter'
       require('@angular/cli/plugins/karma')
     ],
     client:{
