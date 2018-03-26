314761e38dba18d9d6425f76c0228972761bc10d
JD Ryder <jdryder1994@gmail.com>
2018-03-25 22:17:09 -0400

    resetService added, sends and recieves Http with expected results, and added some alerts to notify users of success/failure of register, reset, and login

--- Project2NG/src/app/password-reset/password-reset.component.ts
+++ Project2NG/src/app/password-reset/password-reset.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PasswordResetService } from '../password-reset.service';
 
 @Component({
   selector: 'app-password-reset',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordResetComponent implements OnInit {
 
-  constructor() { }
+  constructor(private resetservice: PasswordResetService) { }
+  private email: string = "";
+
+  public reset(){
+    this.resetservice.reset(this.email);
+  }
 
   ngOnInit() {
   }
