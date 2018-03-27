73a844872f28943408a6c2c9127e39cf920a8317
JD Ryder <jdryder1994@gmail.com>
2018-03-26 20:40:13 -0400

    profile now populates with data from cookie... gender and phone don't because the dummy data is a type mismatch with what the real user object is set up for.

--- Project2NG/src/app/profile/profile.component.ts
+++ Project2NG/src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarService } from '../navbar.service';
 import {User} from '../user';
+import { CookieService } from 'angular2-cookie/services/cookies.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,30 +11,16 @@ import {User} from '../user';
 export class ProfileComponent implements OnInit {
 
   public user:User =new User();
-    // id: number;
-    // firstName: string;
-    // lastName: string;
-    // phone: string;
-    // email: string;
-    // dateOfBirth: string;
-    // gender: string;
   uProfilePic:string = 'http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg';
-
   editMode: boolean = false;
-  constructor(public navbarService:NavbarService) { }
+  constructor(public navbarService:NavbarService, private cookie:CookieService) { }
 
   ngOnInit() {
     this.navbarService.show();
+
+    this.user = <User>this.cookie.getObject('user');
+    console.log(this.user);
     
-    this.user.id=1;
-    this.user.firstName='Hermione';
-    this.user.lastName='Granger';
-    this.user.phone = '123-456-7890';
-    this.user.email='hgranger@hogwwarts.edu';
-    this.user.dateOfBirth = 'noneya'
-    this.user.gender='Female';
-
-
 
 
 
