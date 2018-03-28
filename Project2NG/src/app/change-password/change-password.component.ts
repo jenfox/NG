e6fda080028a1d82d3f5c416ffde6d83d2719cf1
vangjk <johne.vang1@gmail.com>
2018-03-27 22:02:18 -0400

    Vang j (#18)
    * change password
    
    * change pw

---
+++ Project2NG/src/app/change-password/change-password.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {User} from '../user';
+import { CookieService } from 'angular2-cookie/services/cookies.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.css']
+})
+export class ChangePasswordComponent implements OnInit {
+
+  constructor(private cookie:CookieService, private http: HttpClient, private router:Router) { }
+  public oldpassword: string = "";
+  public newpassword: string = "";
+  public confnewpassword: string = "";
+  public user:User= new User;
+  public user2:User = new User;
+
+  ngOnInit() {
+  }
+
+  public checkerCheck(){
+    var checker: boolean = (this.newpassword != this.confnewpassword);
+    return checker;
+  }
+
+  public update(){
+    this.user= <User>this.cookie.getObject('user');
+    if(this.newpassword != this.confnewpassword){
+      alert("Your new passwords do not match.")
+      return;
+    }
+    const data={
+      "email": this.user.email,
+      "password": this.oldpassword
+    }
+    const header= {
+      headers:new HttpHeaders({
+        'Content-Type':'application/json'
+      })
+    }
+    this.http.post('http://localhost:8080/login', data, header).subscribe(
+      (succ:any)=>{
+        console.log(succ);
+        this.user2 = succ;
+
+        if(this.user2.email == this.user.email){
+          const data2={
+            "email": this.user.email,
+            "password": this.newpassword
+          }
+        this.http.post('http://localhost:8080/users/password', data2, header).subscribe(
+          (succ:any)=>{
+            console.log(succ);
+            alert("You have successfully changed your password. ")
+            this.router.navigateByUrl("/home/profile")
+          }
+        )
+      } else {
+        alert("Incorrect Password");
+      }
+      }
+    )
+    
+  }
+
+}
