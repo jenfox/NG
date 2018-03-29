c079c3e24ece057cb03a959c9bfa1aaeb3529880
Joshua Stark <jpstark25@gmail.com>
2018-03-29 00:12:53 -0400

    can like stuff.....

--- Project2NG/src/app/feed/feed.component.ts
+++ Project2NG/src/app/feed/feed.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../Post';
 import { PostService } from '../posts/post.service';
+import { LikePostService } from '../like-post.service';
 
 @Component({
   selector: 'app-feed',
@@ -10,22 +11,43 @@ import { PostService } from '../posts/post.service';
 export class FeedComponent implements OnInit {
 
   posts: Post[];
-  currentUser:{
-    
+  numLikes:number;
+
+  currentUser: {
+
   }
-  constructor(private postService:PostService) { }
+  constructor(private postService: PostService, private likepostservice: LikePostService) { }
 
   ngOnInit() {
     this.getPosts();
   }
 
-  getPosts(){
+  getPosts() {
     this.postService.get20Posts().subscribe(
       posts => this.posts = posts
     );
     // console.log("back in feed.component.oninit");
     // console.log(this.posts[1]);
     // console.log(this.posts);
+  }
+
+  likeThis(postId: number) {
+    console.log("liked " + postId)
+    console.log(this.posts)
+    //if this user is NOT in the array of users that liked the post
+    //add them to the array.
+    //update array size
+    //put arrraysize into numLikes
+
+    this.likepostservice.like(postId).subscribe(
+      post => this.posts.forEach(function (item, i, posts) {
+        if (item.id == post.id)
+          posts[i] = post
+      })
+    );
+
+    
+
   }
 
 }
