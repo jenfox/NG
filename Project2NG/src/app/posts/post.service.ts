ad4702ccc96c14c81169e84ef8114afaa239ba23
Joshua Stark <jpstark25@gmail.com>
2018-03-29 14:02:38 -0400

    posts can now have pictures

--- Project2NG/src/app/posts/post.service.ts
+++ Project2NG/src/app/posts/post.service.ts
@@ -44,32 +44,48 @@ export class PostService {
   // return this.posts;
  }
 
-  createPost(post : String) {
+  createPost(post : String, formdata: FormData) {
 
+    var resp: Post;
+    var poster: Post = new Post(post);
+
+    const url2:string='http://localhost:8080/posts/pictures';
+    this.http.post(url2, formdata).subscribe(
+      (succ:any)=>{
+        console.log(succ);
+        resp = succ;
+        poster.imageUrl = resp.imageUrl;
+        console.log(poster);
+
+        const user = <User>this.cookie.getObject('user');
+        poster.id = user.id;
+        const id = user.id;
+
+        const url = 'http://localhost:8080/posts';
+        const data = {
+          'content' : poster.content,
+          'imageUrl' : poster.imageUrl,
+          'author' : { 'id' : id }
+        };
+
+        const header = {
+          headers : new HttpHeaders({
+            'Content-Type' : 'application/json'
+          })
+        };
+        console.log('data = ' + data.content);
+        this.http.post(url, data).subscribe(
+          (succ:any) => {
+            console.log(succ);
+            
+            // route to the homepage
+            window.location.reload();
+
+            }
+        )
+  })
    // Http request (post)
-   const user = <User>this.cookie.getObject('user');
-   const id = user.id;
-
-   const url = 'http://localhost:8080/posts';
-   const data = {
-     'content' : post,
-     'author' : { 'id' : id }
-   };
-   const header = {
-    headers : new HttpHeaders({
-      'Content-Type' : 'application/json'
-    })
-   };
-   console.log('data = ' + data.author.id);
-   this.http.post(url, data, header).subscribe(
-     (succ:any) => {
-       console.log(succ);
-       
-      // route to the homepage
-       window.location.reload();
-
-      }
-   )
+   
 
 
  }
